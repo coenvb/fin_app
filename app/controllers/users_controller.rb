806650511099cc2c5802819dbcda6c9f490ea122
coenvb <coenvb@hotmail.com>
2018-05-29 22:01:24 -0700

    finished the app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,37 @@ def my_portfolio
 end
 
 def my_friends
-  
+  @friendships = current_user.friends
 end
+
+def search
+  if params[:search_param].blank?
+    flash.now[:danger] = "You entered an empty search string"
+  else
+    @users = User.search(params[:search_param])
+    @users = current_user.except_current_user(@users)
+    flash.now[:danger] = "No users found" if @users.blank?
+  end
+
+  respond_to do |format|
+    format.js { render partial: 'friends/result' }
+  end
+end
+
+def add_friend
+  @friend=User.find(params[:friend])
+  current_user.friendships.build(friend_id: @friend.id)
+  if current_user.save
+    flash[:success] = "Friend was successfully added"
+  else
+    flash[:danger] = "Something went wrong with the friend request"
+  end
+  redirect_to my_friends_path
+end
+
+def show
+  @user = User.find(params[:id])
+  @user_stocks = @user.stocks
+end
+
 end
